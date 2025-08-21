aa7a8a31b03842c93a63fffc2a4fb29fc672caff
Sagarpatil2003 <sp4765699@gmal.com>
2025-08-21 23:20:40 +0530

    create route for StudentDashboard and TeacherDashboard

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Route, Router, Routes } from 'react-router'
 import { UserLogin } from './components/UserLogin'
 import { PageNotFound } from './components/PageNotFound'
 import { SignUp } from './components/SignUp'
+import { StudentDashboard } from './components/StudentDashboard'
+import { TeacherDashboard } from './components/TeacherDashboard'
 
 function App() {
   
@@ -11,7 +13,9 @@ function App() {
     <>
       <Routes>
           <Route path='/' element={<UserLogin/>}></Route>
-          <Route path='signup' element={<SignUp/>}></Route>
+          <Route path='/signup' element={<SignUp/>}></Route>
+          <Route path='/student-dashboard' element={<StudentDashboard/>}></Route>
+          <Route path='/teacher-dashboard' element={<TeacherDashboard/>}></Route>
           <Route path='*' element={<PageNotFound/>}></Route>
           
       </Routes>
